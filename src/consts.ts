fd9c17e5a1dae914591280b74d9a150d022cfcfc
dja98-admin <duncan.adamson@gmail.com>
2026-08-02 16:40:45 -0700

    Set initial project identity

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'WW2 Airplane Engine Analysis with Arduino';
+export const SITE_DESCRIPTION =
+	'A project that analyzes World War II airplane engines from real world data.';
